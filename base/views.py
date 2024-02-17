8f2a4fc7fd9f26129173d8e7e08458940eb14c67
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-02-18 00:22:56 +0545

    configured templates and or extended them to main html file

--- base/views.py
+++ base/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def home (request):
-    return HttpResponse("home page")
+    return render(request,'home.html')
 
 def room(request):
-    return HttpResponse("this is the backrooms")
+    return render(request,'room.html')
--- studybud/urls.py
+++ studybud/urls.py
@@ -5,6 +5,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('base.urls'))
-    
+    path('',include('base.urls')) 
 ]
