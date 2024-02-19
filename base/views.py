2378f3181ddf3840e67d393e4a7e9d0339925bd4
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-02-19 17:55:41 +0545

    connected database to the website

--- base/views.py
+++ base/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
 rooms=[ 
-       {'id':1,'name':'Lets learn python'},
-       {'id':2,'name':'Design with me'},
+       {'id':1,'name':'Lets learn django'},
+       {'id':2,'name':'Lets make some projects'},
        {'id':3,'name':'front end developemnt?'}
     
 ]
 
 def home (request):
+    rooms= Room.objects.all()
     context= {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
